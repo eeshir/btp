ee1b9e5d251332c0bceb786a101d40a3a58df5ff
eeshir <eeshirgupta23@gmail.com>
2024-11-19 17:58:50 +0530

    rollback to hc 2

--- aiproject/settings.py
+++ aiproject/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "aiproject.urls"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'TEMPLATES','static')],
+        'DIRS': [os.path.join(BASE_DIR, 'TEMPLATES')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
